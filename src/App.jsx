de3881a6287d28b43385521b19d65b89091317fa
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2024-10-19 19:35:01 +0530

    navbar, deploy check

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,48 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Kalmesh's Lab</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="navbar flex-column space-btw grow-1">
+        <div className="logo flex-column grow-2 align-center">
+          PLANT KINGDOM
+        </div>
+
+        <div className="links flex-column jend grow-1 align-center">
+          <div>
+            <a href="#" className="no-style">
+              EXPLORE
+            </a>
+          </div>
+          <div>
+            <a href="#" className="no-style">
+              LEARN
+            </a>
+          </div>
+          <div>
+            <a href="#" className="no-style">
+              ARTICLES
+            </a>
+          </div>
+          <div>
+            <a href="#" className="no-style">
+              DOCS
+            </a>
+          </div>
+          <div className="flex-column search-div">
+            <input
+              className="input-l input-search"
+              placeholder="Search"
+              onFocus={() =>
+                document.querySelector(".search-div").classList.add("active")
+              }
+              onBlur={() =>
+                document.querySelector(".search-div").classList.remove("active")
+              }
+            />
+            <span className="material-icons md-36 search-btn">search</span>
+          </div>
+        </div>
       </div>
     </>
   );
