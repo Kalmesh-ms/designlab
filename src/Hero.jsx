ab90d5983e8fccffb1f6eeb9a8b24c7c90f943d1
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2024-10-22 22:11:56 +0530

    initial styles for links

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,7 +3,7 @@ import "./Hero.css";
 export function Hero() {
   return (
     <>
-      <div className="heroText poppins-bold">WELCOME TO KINDOM OF PLANTS</div>
+      <div className="heroText poppins-black">WELCOME TO KINDOM OF PLANTS</div>
     </>
   );
 }
